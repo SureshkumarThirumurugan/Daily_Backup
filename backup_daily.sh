f7839c4579a581a5e3ecef6e4aaf5cfb684ebe13
root <root@localhost.localdomain>
2024-10-01 12:53:21 +0530

    Modifing some comments

--- backup_daily.sh
+++ backup_daily.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# backup the file and archive to store in other location
+# Backup the file and archive it to store in other location
 
 backup_file="/root/realtime_project/support_files/data_backup.txt"
 backup_directory="/dailybackup"
 file_name="backup_$(date +'%d.%m.%Y_%H.%M.%S').tar.gz"
 
+#Checking if the file is present in the directory or not
 if [ -f "$backup_file" ]
 then
-        echo "File is available. Creating backup file"
+        echo "File is available. processing backup"
 else
         echo "File not available. Please check $backup_file"
         exit 1
@@ -16,7 +17,7 @@ fi
 # archive the backup file
 tar -czf "$backup_directory/$file_name" "$backup_file" 2>/dev/null
 
-#check thr bachup was complete
+#check the bachup was complete
 if [ $? -eq 0 ] 
 then
         echo "Backup was completed succesfully"
